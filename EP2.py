6ddc967493f6115798f8cec9d94fdea69608bec2
joaonasser <joaonasser289@hotmail.com>
2015-03-30 18:40:16 -0300

    Commit 1
    iniciando o desenho da forca

---
+++ EP2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 10:58:30 2015
+
+@author:"""
+
+import turtle               # Usa a biblioteca de turtle graphics
+window = turtle.Screen()    # cria uma janela
+window.bgcolor("orange")
+window.title("Forca")
+
+tt  = turtle.Turtle()  # Cria um objeto "desenhador"
+tt.speed(5)  # define a velocidade
+tt.penup()       # Remova e veja o que acontece
+tt.setpos(-300,-100)
+tt.pendown()
+tt.color("purple")
+
+
+
+
